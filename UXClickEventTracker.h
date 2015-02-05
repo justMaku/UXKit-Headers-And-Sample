7306766603bf5dc8f3951b94fe67ab6a09bcb7d7
Michał Kałużny <michalkaluzny@gmail.com>
2015-02-06 00:52:11 +0100

    Remove external imports from UXKit

--- UXClickEventTracker.h
+++ UXClickEventTracker.h
@@ -4,7 +4,9 @@
 //     class-dump is Copyright (C) 1997-1998, 2000-2001, 2004-2013 by Steve Nygard.
 //
 
-#import <UXKit/UXEventTracker.h>
+#import "tTracker.h"
+
+@in"
 
 @interface UXClickEventTracker : UXEventTracker
 {
--- UXTableViewCell.h
+++ UXTableViewCell.h
@@ -4,7 +4,7 @@
 //     class-dump is Copyright (C) 1997-1998, 2000-2001, 2004-2013 by Steve Nygard.
 //
 
-#import <UXKit/UXCollectionViewCell.h>
+#import "UXCollectionViewCell.h"
 
 @class NSColor, NSLayoutConstraint, NSMutableArray, UXLabel, UXView, _UXButton;
 
